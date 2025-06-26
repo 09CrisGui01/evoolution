7a235743d43328343b950106b147ff025de29c2c
Cristian Guilarte <cristian.guilarte@proton.me>
2025-06-26 16:59:09 +0100

    feat: Implement genome

---
+++ source/genome.h
@@ -0,0 +1,43 @@
+#ifndef GENOME
+#define GENOME
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GEN_LENGTH 16
+#define GEN_QUANTITY 8
+
+struct genome {
+        uint8_t DNA[GEN_LENGTH*GEN_QUANTITY];
+        float mutation_rate;
+};
+
+void genome_initialise(struct genome *const restrict genome);
+
+void genome_fprintf(FILE *stream, struct genome *const restrict genome);
+
+#endif //GENOME
+
+#ifdef GENOME_IMPLEMENTATION
+#define genome_at(row, column) (row*GEN_QUANTITY+column)
+
+void genome_initialise(struct genome *restrict genome) {
+      for(size_t i=0; i<GEN_QUANTITY; ++i) {
+        for(size_t j=0; j<GEN_LENGTH; ++j) {
+                genome->DNA[genome_at(i,j)] = rand() % 256;
+        }
+      }
+      genome->mutation_rate = ((float)rand() / (float)(RAND_MAX)) * 100.0;
+}
+
+void genome_fprintf(FILE *stream, struct genome *const restrict genome) {
+      for(size_t i=0; i<GEN_QUANTITY; ++i) {
+        for(size_t j=0; j<GEN_LENGTH; ++j) {
+                fprintf(stream, "%3d ", genome->DNA[genome_at(i,j)]);
+        }
+        fprintf(stream, "\n");
+      }
+}
+
+#endif //GENOME_IMPLEMENTATION
