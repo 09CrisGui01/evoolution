65f10e7062121e75b0d4c04956517660e4c39b3b
Cristian Guilarte <cristian.guilarte@proton.me>
2025-06-28 21:48:20 +0100

    feat: Implement cell

--- source/cell.h
+++ source/cell.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-struct Cell {
+struct cell {
         size_t pid; //Parent ID
         size_t id;
         size_t genome_index;
@@ -11,10 +11,24 @@ struct Cell {
         size_t biomass;
 };
 
+inline struct cell
+cell_generate(size_t const pid, size_t const id, size_t const genome_index,
+              size_t const reserves, size_t const biomass);
+
 #endif //CELL
 
 #ifdef CELL_IMPLEMENTATION
 
-
+inline struct cell
+cell_generate(size_t const pid, size_t const id, size_t const genome_index,
+              size_t const reserves, size_t const biomass) {
+        return (struct cell){
+                .pid = pid,
+                .id = id,
+                .genome_index = genome_index,
+                .reserves = reserves,
+                .biomass = biomass,
+        };
+}
 
 #endif //CELL_IMPLEMENTATION
