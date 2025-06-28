05c680955e2066dcec0a45cdbd6a0817e7f3e045
Cristian Guilarte <cristian.guilarte@proton.me>
2025-06-28 21:54:04 +0100

    test(all): Include arena tests

--- test/all.c
+++ test/all.c
@@ -3,11 +3,13 @@
 
 #include "./genome.h"
 #include "./stack.h"
+#include "./arena.h"
 
 
 int main() {
         test_stack();
         test_genome();
+        test_arena();
         show_results(stderr);
         return 0;
 }
